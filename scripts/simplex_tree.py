429ba05ea35d9126fcca69506aa1fe22c46a1ce2
Chinmay Senapathi <chinmay.k.senapathi@gmail.com>
2025-07-29 18:56:25 -0500

    fixed simplicial complex meet; join requires more data for disjoint labeling; will implement hdvs first

--- scripts/simplex_tree.py
+++ scripts/simplex_tree.py
@@ -1,24 +1,24 @@
 from typing import Dict
-from dataclasses import dataclass
+#from dataclasses import dataclass
 from itertools import combinations
 from copy import copy
 
-@dataclass
-class Simplex:
-    def __init__(self, k=-1, data=[]):
-        assert len(data) == k + 1 
-        self.k : int = k
-        self.data = data
-        #self.label = label
-
+#@dataclass
+#class Simplex:
+#    def __init__(self, k=-1, data=[]):
+#        assert len(data) == k + 1 
+#        self.k : int = k
+#        self.data = data
+#        #self.label = label
+#
 
 # Topological construction with strict and simple rules
 # i.) Any sub-simplex of a simplex in the complex is also an element of the complex
 # ii.) Non-empty intersection (meet) of any 2 simplices is sub-simplex of both simplices
 class SimplicialComplex:
-    def __init__(self, max_grade:int=-1, simplex_store:Dict={}):
+    def __init__(self, max_grade:int=-1, simplex_store=None):
         self.max_grade = max_grade
-        self.k_simplex_store = simplex_store
+        self.k_simplex_store = simplex_store if simplex_store is not None else {}
 
     # Any time simplex is added, recursively add all sub-simplex until 0-simplex
     # Currently stores multiple copies of data; should store one copy and make all
@@ -51,18 +51,34 @@ class SimplicialComplex:
         grade = -1
         simplex_store = {}
         while grade < possible_max_grade:
-            temp_list = []
-            temp_list.extend(self.k_simplex_store[grade+1])
-            temp_list.extend(other_complex.k_simplex_store[grade+1])
-            temp_list  = list(set.intersection(*map(set, temp_list))) 
-            
-            if len(temp_list) == 0:
+            #temp_list = []
+            #temp_list.extend(self.k_simplex_store[grade+1])
+            #temp_list.extend(other_complex.k_simplex_store[grade+1])
+            #temp_list  = list(set.intersection(*map(set, temp_list))) 
+            common = list(set(self.k_simplex_store[grade+1]) & set(other_complex.k_simplex_store[grade+1])) 
+            if len(common) == 0:
                 break
 
             grade += 1
-            simplex_store[grade] = temp_list
+            simplex_store[grade] = common
 
         return SimplicialComplex(grade, simplex_store)
+
+    # Finds the join of 2 simplicial complexes
+    # Returns another simplicial complex
+    # Relies first on the disjoint union
+    # adds additional simplices to (complete a topology)(??)
+    def join(self, other_complex):
+        print("IMPLEMENT")
+        return -1
+        #max_grade = max(self.max_grade, other_complex.max_grade)
+        #joint = SimplicialComplex(grade=max_grade)
+
+        #for self_simplex in self.k_simplex_store.values():
+        #    for other_simplex in other.k_simplex_store.values():
+        #        for s1 in self_simplex:
+        #            for s2 in other_simplex:
+        #                if set(s1).is_disjoint(s2):
 
 if __name__ == "__main__":
     sc = SimplicialComplex()
